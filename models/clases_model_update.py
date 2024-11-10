db0272ce4c32132df562b51ae46d9f13e4a39e5f
Diego Alejandro Hidalgo Machaca <135664425+DiegoH81@users.noreply.github.com>
2024-11-10 11:21:42 -0500

    changes in all tables

--- models/clases_model_update.py
+++ models/clases_model_update.py
@@ -2,15 +2,14 @@ def update_mesa_billar(data_base, mesa_obj):
     cursor = data_base.connection.cursor()
     sql_command = """CALL db_billar.update_mesabillar(%s, %s, %s, %s, %s, %s);"""
     cursor.execute(sql_command, (
-        mesa_obj.id, 
+        mesa_obj.id,
         mesa_obj.tipo,
         mesa_obj.estado, 
-        mesa_obj.id_mantenimiento, 
+        mesa_obj.id_mantenimiento,
         mesa_obj.id_pago_com, 
         mesa_obj.id_ambiente
     ))
     data_base.connection.commit()
-    
 def update_cliente(data_base, cliente_obj):
     cursor = data_base.connection.cursor()
     sql_command = """CALL db_billar.update_cliente(%s, %s, %s, %s, %s, %s, %s);"""
